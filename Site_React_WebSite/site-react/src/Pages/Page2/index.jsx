50594922348d4fe90d991facdffdc541f1c41ace
LeonardoW26 <leonardoww26@gmail.com>
2022-11-11 07:35:42 -0300

    10/11/2022

---
+++ Site_React_WebSite/site-react/src/Pages/Page2/index.jsx
@@ -0,0 +1,13 @@
+import './style.css';
+
+export function Page2() {
+    return (
+        <div id="Page2">
+            <div className="container">
+
+                
+
+            </div>
+        </div>
+    )
+}
---
+++ Site_React_WebSite/site-react/src/Pages/home/index.jsx
@@ -0,0 +1,13 @@
+import './style.css';
+
+export function HomePage() {
+    return (
+        <div id="home-page">
+            <div className="container">
+
+                
+
+            </div>
+        </div>
+    )
+}
