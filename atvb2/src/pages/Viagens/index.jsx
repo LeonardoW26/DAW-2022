a5d06e5e16b4fbff7f69dca63b4ab2f34ebc6e65
LeonardoW26 <leonardoww26@gmail.com>
2022-11-26 14:31:47 -0300

    26/11/2022

--- atvb2/src/pages/Viagens/index.jsx
+++ atvb2/src/pages/Viagens/index.jsx
@@ -1,6 +1,14 @@
 import './style.css';
 
 import img from '../../assets/img/Foz.png';
+import img1 from '../../assets/img/Recanto.webp';
+import img2 from '../../assets/img/curitiba.webp';
+import img3 from '../../assets/img/hermann.jpg';
+import img4 from '../../assets/img/divisor.jpg';
+import img5 from '../../assets/img/salto.jpg';
+import img6 from '../../assets/img/Salvador.jpg';
+import img7 from '../../assets/img/carlos.webp';
+import img8 from '../../assets/img/saopaulo.jpg';
 
 export function ViagensPage() {
     return (
@@ -16,79 +24,82 @@ export function ViagensPage() {
 
                     <div className="card">
                         <p>Recanto das Cachoeiras, PR</p>
-                        <img src="../CSS/img/Recanto.webp" alt="Imagem do Recanto" />
+                        <img src={img1} alt="Imagem do Recanto" />
                     </div>
 
                     <div className="card">
                         <p>Curitiba, PR</p>
-                        <img src="../CSS/img/curitiba.webp" alt="Imagem de CuritibaNelson" />
+                        <img src={img2} alt="Imagem de CuritibaNelson" />
                     </div>
 
                     <div className="card">
                         <p>Cachoeira do Hermann, PR</p>
-                        <img src="../CSS/img/hermann.jpg" alt="Imagem de Hermann" />
+                        <img src={img3} alt="Imagem de Hermann" />
                     </div>
 
                     <div className="card">
                         <p>Morro do Divisor, PR</p>
-                        <img src="../CSS/img/divisor.jpg" alt="Imagem do divisor" />
+                        <img src={img4} alt="Imagem do divisor" />
                     </div>
 
                     <div className="card">
                         <p>Salto São Francisco, PR</p>
-                        <img src="../CSS/img/salto.jpg" alt="Imagem de Salto" />
+                        <img src={img5} alt="Imagem de Salto" />
                     </div>
 
                     <div className="card">
                         <p>Salvador, BA</p>
-                        <img src="../CSS/img/Salvador.jpg" alt="Imagem de Salvador" />
+                        <img src={img6} alt="Imagem de Salvador" />
                     </div>
 
                     <div className="card">
                         <p>Carlos Botelho, SP</p>
-                        <img src="../CSS/img/carlos.webp" alt="Imagem de Carlos" />
+                        <img src={img7} alt="Imagem de Carlos" />
                     </div>
 
                     <div className="card">
                         <p>São Paulo, SP</p>
-                        <img src="../CSS/img/saopaulo.jpg" alt="Imagem de SP" />
+                        <img src={img8} alt="Imagem de SP" />
                     </div>
                 </div>
-            </div>
 
-            <div className="footer">
-                <div className="container">
-                    <div className="footer-content">
-                        <div className="footer-company">
 
-                            <div className="imgFooter">
-                                <img src="../CSS/img/voe-removebg-preview (1).png" alt="Logo" />
+            </div >
+            <div className="footer-viagens">
+                <div className="footer">
+                    <div className="container">
+                        <div className="footer-content">
+                            <div className="footer-company">
+
+                                <div className="imgFooter">
+                                    <img src="../CSS/img/voe-removebg-preview (1).png" alt="Logo" />
+                                </div>
+
+                                <div className="footer-menu">
+                                    <ul>
+                                        <li><a href="#">Sobre nós</a></li>
+                                        <li><a href="#">Especialistas</a></li>
+                                        <li><a href="#">Missão e Visão</a></li>
+                                        <li><a href="#">Nossos Valores</a></li>
+                                    </ul>
+                                </div>
+
+                                <div className="footer-social">
+                                    <span className="Facebook"></span>
+                                    <span className="Twitter"></span>
+                                    <span className="Instagram"></span>
+                                    <span className="Linkedin"></span>
+                                </div>
+
+                                <p className="footer-copyright">
+                                    &#169; 2022 - Todos os direitos reservados
+                                </p>
                             </div>
-
-                            <div className="footer-menu">
-                                <ul>
-                                    <li><a href="#">Sobre nós</a></li>
-                                    <li><a href="#">Especialistas</a></li>
-                                    <li><a href="#">Missão e Visão</a></li>
-                                    <li><a href="#">Nossos Valores</a></li>
-                                </ul>
-                            </div>
-
-                            <div className="footer-social">
-                                <span className="Facebook"></span>
-                                <span className="Twitter"></span>
-                                <span className="Instagram"></span>
-                                <span className="Linkedin"></span>
-                            </div>
-
-                            <p className="footer-copyright">
-                                &#169; 2022 - Todos os direitos reservados
-                            </p>
                         </div>
                     </div>
                 </div>
-
             </div>
-        </div >
+        </div>
+
     )
 }
